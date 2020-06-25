b2677d2841ec914cbb7dcd533220318fb488f685
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-06-25 22:52:22 +0400

    Added main.go and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM golang:1.14.4-alpine3.12
+
+ENV COLOUR blue
+ENV LOAD_TIME 0
+ENV RESPONSE_TIME 0
+ENV PORT 8081
+ENV VERSION 1.0.0
+RUN mkdir /app
+
+ADD *.go /app
+
+WORKDIR /app
+
+RUN go build -o main . && \
+  addgroup -S utils && \
+  adduser -S -g utils utils
+
+USER utils
+
+CMD ["/app/main"]
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+var responseTime, port int
+var colour, version string
+
+func getIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
+func versionHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Duration(responseTime) * time.Second)
+	fmt.Fprintf(w, fmt.Sprintf("<html><body><h1 style=\"color:%v\">%v</h1></body></html>", colour, version))
+	fmt.Printf("%v - /version\n", getIP(r))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Duration(responseTime) * time.Second)
+	fmt.Fprintf(w, "OK")
+	fmt.Printf("%v - /healthz\n", getIP(r))
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("Content-Type", "application/json")
+	resp, _ := json.Marshal(map[string]string{
+		"ip": getIP(r),
+	})
+	fmt.Printf("%v - /ip\n", getIP(r))
+	w.Write(resp)
+}
+
+func handleRequests() {
+	http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/healthz", healthCheckHandler)
+	http.HandleFunc("/ip", ipHandler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func getIntEnvVar(envVar string) int {
+	val, err := strconv.ParseInt(os.Getenv(envVar), 10, 0)
+	if err != nil {
+		fmt.Printf("Error parsing %v\n", envVar)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return int(val)
+}
+
+func getStringEnvVar(envVar string) string {
+	val := os.Getenv(envVar)
+	if val == "" {
+		fmt.Printf("Empty value for %v\n", envVar)
+		os.Exit(1)
+	}
+	return val
+}
+func main() {
+	colour = getStringEnvVar("COLOUR")
+	loadTime := getIntEnvVar("LOAD_TIME")
+	responseTime = getIntEnvVar("RESPONSE_TIME")
+	port = getIntEnvVar("PORT")
+	version = getStringEnvVar("VERSION")
+	fmt.Printf("COLOUR=%v\nLOAD_TIME=%v\nRESPONSE_TIME=%v\nPORT=%v\nVERSION=%v\n", colour, loadTime, responseTime, port, version)
+	time.Sleep(time.Duration(loadTime) * time.Second)
+	handleRequests()
+}
